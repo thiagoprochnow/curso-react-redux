55e34a94df97c8f908a3e650fbedbf49da9852df
Thiago Henrique Prochnow <thiagoh.p@samsung.com>
2021-04-23 17:18:24 -0300

    Compenents children

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
+import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 
 export default _ => {
     return (
@@ -13,6 +15,14 @@ export default _ => {
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
+                <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
+                    <Familia sobrenome="Silva">
+                        <FamiliaMembro nome="Pedro" />
+                        <FamiliaMembro nome="Ana" />
+                        <FamiliaMembro nome="Gustavo" />
+                    </Familia>
+                </Card>
+
                 <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                     <Aleatorio min={1} max={60} />
                 </Card>
---
+++ src/components/basicos/Familia.jsx
@@ -0,0 +1,14 @@
+import React, { cloneElement } from 'react'
+import FamiliaMembro from './FamiliaMembro'
+
+export default props => {
+    return (
+        <div>
+            {
+                props.children.map((child, i) =>  {
+                    return cloneElement(child, { ...props, key: i})
+                })           
+            }
+        </div>
+    )
+}
