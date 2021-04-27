5410b160a6faacfa10d0d372c0a824444e24c799
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2021-04-26 22:13:56 -0300

    Comunicação direta pai filho

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import ListaAlunos from './components/repeticao/ListaAlunos'
 import TabelaProdutos from './components/repeticao/TabelaProdutos'
 import ParOuImpar from "./components/condicional/ParOuImpar"
 import UsuarioInfo from "./components/condicional/UsuarioInfo"
+import "./components/comunicacao/DiretaPai"
+import DiretaPai from './components/comunicacao/DiretaPai'
 
 export default _ => {
     return (
@@ -19,10 +21,15 @@ export default _ => {
             <h1>Fundamentos React</h1>
 
             <div className="Cards">
+                <Card titulo="#09 - Comunicação Direta" color="#982395">
+                    <DiretaPai></DiretaPai>
+                </Card>
+
                 <Card titulo="#08 - Renderização Condicional" color="#982395">
                     <ParOuImpar numero={21}></ParOuImpar>
                     <UsuarioInfo usuario={{ nome: 'Fernando' }}></UsuarioInfo>
-                    <UsuarioInfo usuario={{ email: 'fer@nando.com' }}></UsuarioInfo>
+                    {/* <UsuarioInfo usuario={{}}></UsuarioInfo>
+                    <UsuarioInfo></UsuarioInfo> */}
                 </Card>
 
                 <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
